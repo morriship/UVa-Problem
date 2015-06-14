494c4ca16c58ebb54fb53eb3bc21212ed57d4dfa
aytawgf <snakeooxx@gmail.com>
2015-06-14 14:22:08 +0800

    Basic

---
+++ 12704 - Little Masters.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+#define two(a) (1<<(a))
+#define LINF (1ll<<61)
+#define EPS (1e-14)
+#define Lshift(a,b) (a<<b)
+#define Rshift(a,b) (a>>b)
+#define rep(a,b) for(a=0 ; a<b ; a++)
+#define xrep(a,b,c) for(a=b ; a<c ; a++)
+#define INF (1<<29)
+#define swap(a,b) ( (a^=b) , (b^=a) , (a^=b) )
+#define min(a,b) ( a < b ? a : b )
+#define max(a,b) ( a > b ? a : b )
+#define GET(x) (mark[x>>5]>>(x&31)&1)
+#define SET(x) (mark[x>>5]|=1<<(x&31))
+#define maxL (10000000>>5)+1
+#define mod 1000000007
+typedef long long ll;
+using namespace std;
+
+
+int main() {
+    int t;
+    double x, y, r;
+    for (cin >> t; t; t--) {
+        cin >> x >> y >> r;
+        double tmp = sqrt((x*x)+(y*y));
+        printf("%.2lf %.2lf\n", r-tmp+EPS, tmp+r+EPS);
+    }
+}
