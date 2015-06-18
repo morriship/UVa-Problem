c51788c1275629bc097fd3e059c67da7150e8d29
aytawgf <snakeooxx@gmail.com>
2015-06-19 00:35:57 +0800

    Math

---
+++ 10790 - How Many Points of Intersection?.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+#define two(a) (1<<(a))
+#define LINF (1ll<<61)
+#define EPS (1e-14)
+#define Lshift(a,b) (a<<b)
+#define Rshift(a,b) (a>>b)
+#define rep(a,b) for(a=0 ; a<b ; a++)
+#define xrep(a,b,c) for(a=b ; a<c ; a++)
+#define INF (1<<29)
+#define GET(x) (mark[x>>5]>>(x&31)&1)
+#define SET(x) (mark[x>>5]|=1<<(x&31))
+#define maxL (10000000>>5)+1
+#define mod 1000000007
+typedef long long ll;
+using namespace std;
+
+
+int main() {
+    int a, b, t(1);
+    while (scanf("%d%d", &a, &b) && a) {
+        cout << "Case " << t++ << ": " << (ll)a*(a-1)/2*b*(b-1)/2 << endl; 
+    }
+}
