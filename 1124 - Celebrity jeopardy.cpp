b4df8238b5ae20a61d20388f6b2dea0255ed5dfd
aytawgf <snakeooxx@gmail.com>
2015-06-21 21:57:31 +0800

    Very Easy

---
+++ 1124 - Celebrity jeopardy.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+#define two(a) (1<<(a))
+#define LINF (1ll<<61)
+#define EPS (1e-14)
+#define Lshift(a,b) (a<<b)
+#define Rshift(a,b) (a>>b)
+#define rep(a,b) for(a=0 ; a<b ; a++)
+#define xrep(a,b,c) for(a=b ; a<c ; a++)
+#define INF (1<<29)
+#define GET(x) (mark[x>>5]>>(x&31)&1)
+#define SET(x) (mark[x>>5]|=1<<(x&31))
+#define maxL (10000000>>5)+1
+#define mod 1000000007
+typedef long long ll;
+using namespace std;
+
+char in[1000];
+
+int main() {
+    while (cin.getline(in, 1000)) { cout << in << endl; }
+}
